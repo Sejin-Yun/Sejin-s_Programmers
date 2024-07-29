9880cc1c1010c31b1572380facff134d5a2e32f3
Sejin Yun <84021030+Sejin-Yun@users.noreply.github.com>
2024-07-30 02:56:27 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    int n;
+    char c;
+    stringstream ss(my_string);
+    ss >> answer;
+    
+    while(ss >> c >> n)
+    {
+        if(c == '+') 
+            answer += n;
+        else 
+            answer -= n;
+    }
+    
+    return answer;
+}
